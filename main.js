5f0ee23ce86c1f254dfbf01fe2cc603272101e43
jesus <jesushv875@gmail.com>
2021-01-07 17:22:48 -0600

    commit message

--- main.js
+++ main.js
@@ -19,4 +19,3 @@ $('section.awSlider .carousel').on('slid.bs.carousel', function () {
  var bscn = $(this).find('.item.active > img').attr('src');
 	$('section.awSlider > img').attr('src',bscn);
 });
-
